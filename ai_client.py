f5b7df881ec7b6a249a8c792ef9078670f9fe1bf
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2025-07-26 02:59:46 -0500

    chages to dotenv and removes aws secrets

--- ai_client.py
+++ ai_client.py
@@ -1,11 +1,15 @@
 
 from google import genai
 from google.genai import types
-from aws_secret import get_secret
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
 
 class AiBot():
     def __init__(self):
-        self.api_key = get_secret()
+        self.api_key = os.environ.get("GEMINI_API_KEY")
         self.client = genai.Client(api_key=self.api_key)
         self.model = "gemini-2.0-flash-lite"
         self.system_instruction = "You are a job interviewer. You are interviewing the person for the job."
