b0a4ea27dd93b7e7a2f603f9796b3be8007854d7
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2025-07-26 19:57:20 -0500

    adds thinking config and changes prompts

--- ai_client.py
+++ ai_client.py
@@ -3,6 +3,7 @@ from google import genai
 from google.genai import types
 import os
 from dotenv import load_dotenv
+from constants import SYSTEM_INSTRUCTION, MODEL
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,10 +12,10 @@ class AiBot():
     def __init__(self):
         self.api_key = os.environ.get("GEMINI_API_KEY")
         self.client = genai.Client(api_key=self.api_key)
-        self.model = "gemini-2.0-flash-lite"
-        self.system_instruction = "You are a job interviewer. You are interviewing the person for the job."
+        self.model = MODEL
+        self.system_instruction = SYSTEM_INSTRUCTION
         self.conversation = {"prompts": [], "answers": [] }
-        self.chat = self.client.chats.create(model=self.model, config=types.GenerateContentConfig(system_instruction=self.system_instruction))
+        self.chat = self.client.chats.create(model=self.model, config=types.GenerateContentConfig(thinking_config=types.ThinkingConfig(thinking_budget=1024)))
 
     def converse(self, prompt):
         
@@ -22,9 +23,10 @@ class AiBot():
 
         return response.text
     
-    def summarize_text(self, job_text):
+    def get_job_title_and_company(self, job_text):
 
-        contents = f"Summarize this job description in less than one sentence with the company name and title: {job_text}"
+        contents = f"Provide the company name and job title in: {job_text}. If there is no company name then just provide the job title. Do not add any other comments. \
+        If there is a company name provide it in the form: job title at company name. If there is no company name then just provide the job title"
 
         response = self.client.models.generate_content(model=self.model, contents=contents)
 
